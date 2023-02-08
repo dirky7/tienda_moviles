4c43c4cc5a2d4d21f2339a2db83dcc0270006ac0
jesulito29 <74043073+jesulito29@users.noreply.github.com>
2023-02-08 19:29:19 +0100

    estilo incorporado

--- index.php
+++ index.php
@@ -4,20 +4,20 @@ include "config/funciones.php";
 if (isset($_POST['inicioSesion'])) {
 	//almacenamos el usuario y clave
 	$login = $_POST['usuario'];
-    $password = $_POST['password'];
+	$password = $_POST['password'];
 
 	//si estan vacios
 	if (empty($login) || empty($password)) {
 		$error = "Debes introducir un nombre de usuario y una contraseña";
 		//si no estan vacios
 	} else {
-		if(existeLogin($login)) {
-			if(comprobarInicioSesion($login, $password)) {
+		if (existeLogin($login)) {
+			if (comprobarInicioSesion($login, $password)) {
 				session_start();
-                $_SESSION['usuario']=$login;
-                $_SESSION['hora']=date("H:i",time());
-                //Nos redirige a la pagina usuario
-                header("Location:gestion.php");
+				$_SESSION['usuario'] = $login;
+				$_SESSION['hora'] = date("H:i", time());
+				//Nos redirige a la pagina usuario
+				header("Location:tecnico/gestion.php");
 			} else {
 				$error = "Datos incorrectos";
 			}
@@ -30,40 +30,52 @@ if (isset($_POST['inicioSesion'])) {
 
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="UTF-8">
 	<title>Contabilidad Personal</title>
-	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+	<link rel="stylesheet" type="text/css" href="css/inicio.css">
 </head>
-<body id="pagina-login">
-	<header>
-		<h1>Gastos Personales</h1>
-	</header>
-	<nav>Contabilidad personal</nav>
-	<main>
-		<fieldset class="mini-formulario">
-			<legend>Iniciar Sesión</legend>
-			<?php
-			if (isset($error)) {
-				echo "<div class='error'>$error</div>";
-			}
-			?>
-			<form method="post">
-				<div class="input-labeled">
-					<label>Usuario:</label>
-					<input type="text" name="usuario" required maxlength="10">
+
+<body>
+	<form method="post">
+		<div class="login-wrap">
+			<div class="login-html">
+				<label for="tab-1" class="tab"><a href="index.php">Inicio Sesion</a></label>
+				<label for="tab-2" class="tab"><a href="registro.php">Registro</a></label>
+				<div class="login-form">
+					<div class="sign-in-htm">
+						<div class="group">
+							<label for="user" class="label">Usuario</label>
+							<input id="user" name="usuario" required maxlength="20" type="text" class="input">
+						</div>
+						<div class="group">
+							<label for="pass" class="label">Contraseña</label>
+							<input id="pass" type="password" name="password" required maxlength="20" class="input" data-type="password">
+						</div>
+						<div class="group">
+							<input id="check" type="checkbox" class="check" checked>
+							<label for="check"><span class="icon"></span> Mostrar contraseña</label>
+						</div>
+						<div class="group">
+							<input type="submit" class="button" name="inicioSesion" value="Inicar Sesion">
+						</div>
+						<div class="error">
+							<?php
+							if (isset($error)) {
+								echo "<div class='error'>$error</div>";
+							}
+							?>
+						</div>
+						<div class="hr">
+						</div>
+						<div class="foot-lnk">
+							<a href="#forgot">¿Has olvidado tu contraseña?</a>
+						</div>
+					</div>
 				</div>
-				<div class="input-labeled">
-					<label>Contraseña:</label>
-					<input type="password" name="password" required maxlength="20">
-				</div>
-				<input type="submit" name="inicioSesion" value="Iniciar sesion">
-				<hr>
-				<a href='registro.php'>Registrarse</a>
-				<a href='invitado.php'>Entrar como invitado</a>
-				<input type="hidden" name="sesion">
-			</form>
-		</fieldset>
-	</main>
+			</div>
+		</div>
+	</form>
 </body>
 </html>
--- registro.php
+++ registro.php
@@ -1,7 +1,7 @@
 <?php
 include("config/funciones.php");
 //Comprobamos si ya se enviado el formulario
-if (isset($_POST['enviar'])) {
+if (isset($_POST['registro'])) {
     //guardamos los datos del formulario
     $nombre = $_POST['nombre'];
     $apellidos = $_POST['apellidos'];
@@ -14,26 +14,26 @@ if (isset($_POST['enviar'])) {
     //Comprobamos si algun campo vacio
     if (empty($nombre) || empty($apellidos) || empty($login) || empty($password) || empty($password2) || empty($email)) {
         $error = $error . "Debe de completar todos los campos.<br>";
-    //si no hay ningun campo vacio
+        //si no hay ningun campo vacio
     } else {
         //si no existe el login
-        if(!existeLogin($login)) {
+        if (!existeLogin($login)) {
             //si la validacion de la contraseña es valido
-            if(validacionContraseya($password, $password) == "") {
+            if (validacionContraseya($password, $password2) == "") {
                 //si la validaicion del email es valido
-                if(validar_email($email) == "") {
+                if (validar_email($email) == "") {
                     anyadirTecnicoJson($nombre, $apellidos, $login, $password, $email);
                     $anuncio = "Usuario registrado correctamente";
-                    echo("<meta http-equiv='refresh' content='1;url=index.php'>");
-                //si la validacion del email es incorrecta
+                    echo ("<meta http-equiv='refresh' content='1;url=index.php'>");
+                    //si la validacion del email es incorrecta
                 } else {
                     $error = validar_email($email);
                 }
-            //si la validacion de la contraseña es incorrecta
+                //si la validacion de la contraseña es incorrecta
             } else {
                 $error = validacionContraseya($password, $password2);
             }
-        //si existe el usuario
+            //si existe el usuario
         } else {
             $error = "Ya existe el nombre de usuario";
         }
@@ -46,58 +46,61 @@ if (isset($_POST['enviar'])) {
 <!-- Tema 3 : Desarrollo de aplicaciones web con PHP -->
 <!-- Tarea 3, Foro: registro.php -->
 <html>
+
 <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <title>Foro DWES</title>
-    <link href="css/registro.css" rel="stylesheet" type="text/css">
+    <link href="css/inicio.css" rel="stylesheet" type="text/css">
 </head>
+
 <body>
-    <div id='registro'>
-        <form action='registro.php' method='post'>
-            <fieldset>
-                <legend>Registro de nuevo usuario</legend>
-                <div>
-                    <?php
-                    if (isset($error)) {
-                        echo "<span class='error'>" . $error . "</span>";
-                    }
-                    if (isset($anuncio)) {
-                        echo "<span class='anuncio'>" . $anuncio . "</span>";
-                    }
-                    ?>
+    <form method="post">
+        <div class="login-wrap">
+            <div class="login-html">
+                <label for="tab-1" class="tab"><a href="index.php">Inicio Sesion</a></label>
+                <label for="tab-2" class="tab"><a href="registro.php">Registro</a></label>
+                <!-- Formulario de registro -->
+                <div class="login-form">
+                    <div class="sign-up-htm">
+                        <div class="group">
+                            <label for="user" class="label">Nombre</label>
+                            <input id="user" type="text" name="nombre" class="input" maxlength="50" value="<?php if (isset($_POST['nombre'])) echo $_POST['nombre']; ?>" required>
+                        </div>
+                        <div class="group">
+                            <label for="apell" class="label">Apellidos</label>
+                            <input class="input" id="apell" type="text" name='apellidos' id='apellidos' maxlength="50" value="<?php if (isset($_POST['apellidos'])) echo $_POST['apellidos']; ?>" required>
+                        </div>
+                        <div class="group">
+                            <label for="usu" class="label">Nombre de usuario</label>
+                            <input class="input" type='text' name='usuario' id='usuario' maxlength="50" value="<?php if (isset($_POST['usuario']) && !existeLogin($login)) echo $_POST['usuario']; ?>" required>
+                        </div>
+                        <div class="group">
+                            <label for="pass" class="label">Contraseña</label>
+                            <input id="pass" type="password" class="input" data-type="password" name="password" maxlength="50" required>
+                        </div>
+                        <div class="group">
+                            <label for="pass" class="label">Repite la contraseña</label>
+                            <input id="pass" type="password" class="input" data-type="password" name="password2" maxlength="50" required>
+                        </div>
+                        <div class="group">
+                            <label for="pass" class="label">Email</label>
+                            <input id="pass" type="text" class="input" name="email" maxlength="50" value="<?php if (isset($_POST['email']) && (validar_email($email) == "")) echo $_POST['email']; ?>" required>
+                        </div>
+                        <div class="group">
+                            <input type="submit" class="button" name="registro" value="Crear cuenta">
+                        </div>
+                        <div class="error">
+							<?php
+							if (isset($error)) {
+								echo "<div class='error'>$error</div>";
+							}
+							?>
+						</div>
+                    </div>
                 </div>
-                <div class='campo'>
-                    <label for='usuario'>Nombre:</label><br />
-                    <input type='text' name='nombre' id='nombre' maxlength="50" value="<?php if (isset($_POST['nombre'])) echo $_POST['nombre']; ?>" /><br />
-                </div>
-                <div class='campo'>
-                    <label for='usuario'>Apellidos:</label><br />
-                    <input type='text' name='apellidos' id='apellidos' maxlength="50" value="<?php if (isset($_POST['apellidos'])) echo $_POST['apellidos']; ?>" /><br />
-                </div>
-                <div class='campo'>
-                    <label for='usuario'>Nombre de usuario:</label><br />
-                    <input type='text' name='usuario' id='usuario' maxlength="50" value="<?php if (isset($_POST['usuario']) && !existeLogin($login)) echo $_POST['usuario']; ?>"/><br />
-                </div>
-                <div class='campo'>
-                    <label for='password'>Contraseña:</label><br />
-                    <input type='password' name='password' id='password' maxlength="50" /><br />
-                </div>
-                <div class='campo'>
-                    <label for='password'>Escriba de nuevo la Contraseña:</label><br />
-                    <input type='password' name='password2' id='password2' maxlength="50" /><br />
-                </div>
-                <div class='campo'>
-                    <label for='email'>Email:</label><br />
-                    <input type='text' name='email' id='email' maxlength="50" value="<?php if (isset($_POST['email']) && (validar_email($email) == "")) echo $_POST['email']; ?>" /><br />
-                </div>
-                <div class='campo'>
-                    <input type='submit' name='enviar' value='Enviar' />
-                </div>
-                <div class='campo'>
-                    <input type='button' name='volver' value='Volver' onClick="location.href='index.php'" />
-                </div>
-        </form>
-        </fieldset>
-    </div>
+            </div>
+        </div>
+        </div>
+    </form>
 </body>
 </html>
