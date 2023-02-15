a5276350cfa4368095959dd2118b8d840ff746bf
dirky7 <dhurper0905@g.educaand.es>
2023-02-15 04:52:00 +0100

    restricción de las opciones con cada usuario

--- inicio.php
+++ inicio.php
@@ -21,8 +21,7 @@ comprobarSiEstaLogeado();
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-red navbar-dark">
-        <div class="wrapper">
-        </div>
+        <div class="wrapper"></div>
         <div class="container-fluid all-show">
             <a class="navbar-brand" href="inicio.php">Tienda Reparación Móviles <i class="fa fa-codepen"></i></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -59,18 +58,53 @@ comprobarSiEstaLogeado();
 
     <main>
         <ul class="card-wrapper">
-            <li class="card">
-                <img src='https://images.unsplash.com/photo-1611916656173-875e4277bea6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHw&ixlib=rb-1.2.1&q=80&w=400' alt=''>
-                <a href="cliente/insertar.php"><input type="button" value="Insertar Incidencia"></a>
-            </li>
-            <li class="card">
-                <img src='https://images.unsplash.com/photo-1611083360739-bdad6e0eb1fa?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHw&ixlib=rb-1.2.1&q=80&w=400' alt=''>
-                <a href="cliente/gestion.php"><input type="button" value="Gestionar Incidencia"></a>
-            </li>
-            <li class="card">
-                <img src='https://images.unsplash.com/photo-1613230485186-2e7e0fca1253?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHw&ixlib=rb-1.2.1&q=80&w=400' alt=''>
-                <a href=""><input type="button" value="Gestionar Usuarios"></a>
-            </li>
+            <?php
+			
+				if ($_SESSION['usuario'] == "")
+				{
+					echo "
+					<li class='card'>
+						<img src='https://images.unsplash.com/photo-1611916656173-875e4277bea6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHw&ixlib=rb-1.2.1&q=80&w=400' alt=''>
+						<a href='cliente/insertar.php'>
+							<input type='button' value='Insertar Incidencia'>
+						</a>
+					</li>
+					";
+				}
+				else
+				{
+					//usuario: admin
+					//password: Adminjdm1
+					if ($_SESSION['usuario'] == "admin")
+					{
+						echo "
+						<li class='card'>
+							<img src='https://images.unsplash.com/photo-1611083360739-bdad6e0eb1fa?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHw&ixlib=rb-1.2.1&q=80&w=400' alt=''>
+							<a href='cliente/gestion.php'>
+								<input type='button' value='Gestionar Incidencia'>
+							</a>
+						</li>
+						<li class='card'>
+							<img src='https://images.unsplash.com/photo-1613230485186-2e7e0fca1253?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHw&ixlib=rb-1.2.1&q=80&w=400' alt=''>
+							<a href=''>
+								<input type='button' value='Gestionar Usuarios'>
+							</a>
+						</li>";
+					}
+					else
+					{
+						echo "
+						<li class='card'>
+							<img src='https://images.unsplash.com/photo-1611083360739-bdad6e0eb1fa?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHw&ixlib=rb-1.2.1&q=80&w=400' alt=''>
+							<a href='cliente/gestion.php'>
+								<input type='button' value='Gestionar Incidencia'>
+							</a>
+						</li>
+						";
+					}
+				}
+			
+			?>
         </ul>
     </main>
 </body>
